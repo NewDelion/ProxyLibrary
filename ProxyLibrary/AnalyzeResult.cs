d1d1dfda6d147584473beeeac56c65045193ae69
NewDelion <el.emena.crea@gmail.com>
2016-02-26 23:30:30 +0900

    Release commit

---
+++ ProxyLibrary/AnalyzeResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ProxyLibrary
+{
+    public class AnalyzeResult
+    {
+        public enum Command
+        {
+            Send        = 0x01,
+            Cancel      = 0x02
+        }
+
+        public Command result = Command.Send;
+        public Packet packet;
+
+        public AnalyzeResult(Command result, Packet packet)
+        {
+            this.result = result;
+            this.packet = packet;
+        }
+    }
+}
