da75bf346d3dd7afedb1ebeaf1219a6e55b9650e
NewDelion <el.emena.crea@gmail.com>
2016-02-26 23:37:05 +0900

    Sample commit

---
+++ ProxyLibrary_sample1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyLibrary_sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy(19132);
+            proxy.addServer(new ProxyLibrary.Server("127.0.0.1", 19133));
+            proxy.Start();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "stop")
+                {
+                    if (proxy.isEnable())
+                    {
+                        proxy.Stop();
+                        Console.WriteLine("プロキシを停止しました。");
+                    }
+                    else
+                    {
+                        Console.WriteLine("プロキシは停止中です。");
+                    }
+                }
+                else if (command == "start")
+                {
+                    if (!proxy.isEnable())
+                    {
+                        proxy.Start();
+                        Console.WriteLine("プロキシを起動しました。");
+                    }
+                    else
+                    {
+                        Console.WriteLine("プロキシは稼働中です。");
+                    }
+                }
+                else if (command == "quit")
+                {
+                    if (proxy.isEnable())
+                        proxy.Stop();
+                    break;
+                }
+                else if (command == "clear")
+                {
+                    Console.Clear();
+                }
+            }
+            Console.WriteLine();
+            Console.Write("press any key...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProxyLibrary_sample1/Proxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ProxyLibrary;
+
+namespace ProxyLibrary_sample1
+{
+    public class Proxy : ProxyBase
+    {
+        public Proxy(int port)
+        {
+            this.proxy_port = port;
+        }
+
+        public override AnalyzeResult AnalyzeServer(Client client, Packet packet)
+        {
+            Console.WriteLine("{0}:{1} => {2}:{3}", packet.address.Address.ToString(), packet.address.Port, client.ip, client.port);
+            
+            return base.AnalyzeServer(client, packet);
+        }
+
+        public override AnalyzeResult AnalyzeClient(Client client, Packet packet)
+        {
+            Console.WriteLine("{0}:{1} => {2}:{3}", client.ip, client.port, client.ConnectingServer.ip, client.ConnectingServer.port);
+            
+            return base.AnalyzeClient(client, packet);
+        }
+    }
+}
